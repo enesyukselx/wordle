d9db694ff2921fc5ae58cdacfd0a8b01b204f251
Enes Yüksel <enesyuksel9903@gmail.com>
2022-09-04 01:44:06 +0300

    Game.js Update

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,13 +1,53 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import useKeypress from "react-use-keypress";
 
 import classes from "./Game.module.css";
 import Tile from "./Tile";
 
 const Game = () => {
-    const DUMMY_ANSWER = "kirpi";
+    const DUMMY_ANSWER = "fener";
 
-    const [isEntered, setIsEntered] = useState(false);
+    const availableLetters = [
+        "e",
+        "r",
+        "t",
+        "y",
+        "u",
+        "ı",
+        "o",
+        "p",
+        "ğ",
+        "ü",
+        "a",
+        "s",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        "ş",
+        "i",
+        "z",
+        "c",
+        "v",
+        "b",
+        "n",
+        "m",
+        "ö",
+        "ç",
+    ];
+    const [isEntered, setIsEntered] = useState([
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+    ]);
     const [tileCount, setTileCount] = useState(0);
+    const [rowCount, setRowCount] = useState(0);
 
     const [row1, setRow1] = useState(["", "", "", "", ""]);
     const [row2, setRow2] = useState(["", "", "", "", ""]);
@@ -16,17 +56,45 @@ const Game = () => {
     const [row5, setRow5] = useState(["", "", "", "", ""]);
     const [row6, setRow6] = useState(["", "", "", "", ""]);
 
-    useEffect(() => {
-        document.addEventListener("keydown", keyDownHandler);
-    }, []);
+    const setRows = [setRow1, setRow2, setRow3, setRow4, setRow5, setRow6];
 
-    const keyDownHandler = (event) => {
-        setRow1((prevState) => {
-            const updatedRow = [...prevState];
-            updatedRow[tileCount] = event.key.toUpperCase();
-            return updatedRow;
+    useKeypress("Enter", () => {
+        if (tileCount === 5 && rowCount !== 6) {
+            setRowCount(rowCount + 1);
+            setIsEntered((prevState) => {
+                const updatedArr = [...prevState];
+                updatedArr[rowCount] = true;
+                return updatedArr;
+            });
+            setTileCount(0);
+            if (rowCount === 5) {
+                setTileCount(5);
+            }
+        }
+    });
+
+    useKeypress("Backspace", () => {
+        if (tileCount !== 0) {
+            setTileCount((prevState) => prevState - 1);
+            setRows[rowCount]((prevState) => {
+                const updatedArr = [...prevState];
+                updatedArr[tileCount - 1] = "";
+                return updatedArr;
+            });
+        }
+    });
+
+    useKeypress(availableLetters, (e) => {
+        if (tileCount === 5) {
+            return;
+        }
+        setRows[rowCount]((prevRow1) => {
+            const newRow1 = [...prevRow1];
+            newRow1[tileCount] = e.key;
+            return newRow1;
         });
-    };
+        setTileCount((prevTileCount) => prevTileCount + 1);
+    });
 
     const wrong = (index, row) => {
         const answerLetter = DUMMY_ANSWER[index];
@@ -69,9 +137,12 @@ const Game = () => {
                         key={index}
                         letter={letter}
                         {...(!empty(letter) &&
+                            isEntered[0] &&
                             wrong(index, row1) && { wrong: true })}
-                        {...(present(index, row1) && { present: true })}
-                        {...(correct(index, row1) && { correct: true })}
+                        {...(isEntered[0] &&
+                            present(index, row1) && { present: true })}
+                        {...(isEntered[0] &&
+                            correct(index, row1) && { correct: true })}
                     />
                 ))}
             </div>
@@ -81,9 +152,12 @@ const Game = () => {
                         key={index}
                         letter={letter}
                         {...(!empty(letter) &&
+                            isEntered[1] &&
                             wrong(index, row2) && { wrong: true })}
-                        {...(present(index, row2) && { present: true })}
-                        {...(correct(index, row2) && { correct: true })}
+                        {...(isEntered[1] &&
+                            present(index, row2) && { present: true })}
+                        {...(isEntered[1] &&
+                            correct(index, row2) && { correct: true })}
                     />
                 ))}
             </div>
@@ -93,9 +167,12 @@ const Game = () => {
                         key={index}
                         letter={letter}
                         {...(!empty(letter) &&
+                            isEntered[2] &&
                             wrong(index, row3) && { wrong: true })}
-                        {...(present(index, row3) && { present: true })}
-                        {...(correct(index, row3) && { correct: true })}
+                        {...(isEntered[2] &&
+                            present(index, row3) && { present: true })}
+                        {...(isEntered[2] &&
+                            correct(index, row3) && { correct: true })}
                     />
                 ))}
             </div>
@@ -105,9 +182,12 @@ const Game = () => {
                         key={index}
                         letter={letter}
                         {...(!empty(letter) &&
+                            isEntered[3] &&
                             wrong(index, row4) && { wrong: true })}
-                        {...(present(index, row4) && { present: true })}
-                        {...(correct(index, row4) && { correct: true })}
+                        {...(isEntered[3] &&
+                            present(index, row4) && { present: true })}
+                        {...(isEntered[3] &&
+                            correct(index, row4) && { correct: true })}
                     />
                 ))}
             </div>
@@ -117,9 +197,12 @@ const Game = () => {
                         key={index}
                         letter={letter}
                         {...(!empty(letter) &&
+                            isEntered[4] &&
                             wrong(index, row5) && { wrong: true })}
-                        {...(present(index, row5) && { present: true })}
-                        {...(correct(index, row5) && { correct: true })}
+                        {...(isEntered[4] &&
+                            present(index, row5) && { present: true })}
+                        {...(isEntered[4] &&
+                            correct(index, row5) && { correct: true })}
                     />
                 ))}
             </div>
@@ -129,9 +212,12 @@ const Game = () => {
                         key={index}
                         letter={letter}
                         {...(!empty(letter) &&
+                            isEntered[5] &&
                             wrong(index, row6) && { wrong: true })}
-                        {...(present(index, row6) && { present: true })}
-                        {...(correct(index, row6) && { correct: true })}
+                        {...(isEntered[5] &&
+                            present(index, row6) && { present: true })}
+                        {...(isEntered[5] &&
+                            correct(index, row6) && { correct: true })}
                     />
                 ))}
             </div>
