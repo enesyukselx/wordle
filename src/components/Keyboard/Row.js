2a9f58d70fa1ec87156996fbf88022bf24cb3335
Enes Yüksel <enesyuksel9903@gmail.com>
2022-09-07 05:31:35 +0300

    Keys correct wrong present states

--- src/components/Keyboard/Row.js
+++ src/components/Keyboard/Row.js
@@ -1,8 +1,59 @@
+import { useState, useContext, useEffect } from "react";
+
+import Context from "../../store/context";
 import classes from "./Row.module.css";
 
 import Key from "./Key";
 
 const Row = (props) => {
+    const ctx = useContext(Context);
+
+    const [correctLetters, setCorrectLetters] = useState([]);
+    const [wrongLetters, setWrongLetters] = useState([]);
+    const [presentLetters, setPresentLetters] = useState([]);
+
+    const checkKeyStatus = () => {
+        if (ctx.isEntered[ctx.rowCount - 1]) {
+            ctx.WORDLE_ANSWER.split("").forEach((letter, index) => {
+                if (
+                    ctx.rows[ctx.rowCount - 1][index].toLowerCase() ===
+                    letter.toLowerCase()
+                ) {
+                    setCorrectLetters((prevState) => {
+                        const updatedArr = [...prevState];
+                        updatedArr.push(letter);
+                        return updatedArr;
+                    });
+                }
+
+                if (
+                    ctx.rows[ctx.rowCount - 1][index].toLowerCase() !==
+                    letter.toLowerCase()
+                ) {
+                    setWrongLetters((prevState) => {
+                        const updatedArr = [...prevState];
+                        updatedArr.push(
+                            ctx.rows[ctx.rowCount - 1][index].toLowerCase()
+                        );
+                        return updatedArr;
+                    });
+                }
+
+                if (ctx.rows[ctx.rowCount - 1].includes(letter)) {
+                    setPresentLetters((prevState) => {
+                        const updatedArr = [...prevState];
+                        updatedArr.push(letter);
+                        return updatedArr;
+                    });
+                }
+            });
+        }
+    };
+
+    useEffect(() => {
+        checkKeyStatus();
+    }, [ctx.isEntered]);
+
     return (
         <div className={classes.Row}>
             {props.enter ? (
@@ -18,9 +69,16 @@ const Row = (props) => {
                         key={letter}
                         letter={letter}
                         clickHandler={props.keyClickHandler}
-                        correct={props.correct}
-                        wrong={props.wrong}
-                        present={props.present}
+                        correct={correctLetters.includes(letter)}
+                        wrong={
+                            !correctLetters.includes(letter) &&
+                            !presentLetters.includes(letter) &&
+                            wrongLetters.includes(letter)
+                        }
+                        present={
+                            !correctLetters.includes(letter) &&
+                            presentLetters.includes(letter)
+                        }
                     />
                 );
             })}
